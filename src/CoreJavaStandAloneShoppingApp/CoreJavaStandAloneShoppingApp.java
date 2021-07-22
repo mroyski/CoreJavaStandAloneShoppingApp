70e83978079e112ba36d19e5495e06b5568dad9c
Mike Royski <mroyski@gmail.com>
2021-07-22 11:33:56 -0400

    adding register method

--- src/CoreJavaStandAloneShoppingApp/CoreJavaStandAloneShoppingApp.java
+++ src/CoreJavaStandAloneShoppingApp/CoreJavaStandAloneShoppingApp.java
@@ -3,12 +3,42 @@ package CoreJavaStandAloneShoppingApp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class CoreJavaStandAloneShoppingApp {
 
 	public static void main(String[] args)
 			throws IOException
 {
+		Home(new User());
+		
+
+
+	}
+	
+	public static void Register() 
+			throws IOException
+	{
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(System.in));
+		System.out.println("REGISTER\n");
+		
+		System.out.println("Enter a valid email address:");
+		String email = reader.readLine();
+		
+		System.out.println("Enter your password");
+		String password = reader.readLine();
+		
+		User newUser = new User(email, password);
+		Users.AllUsers.add(newUser);
+		
+		Home(newUser);
+	}
+	
+	public static void Home(User user)
+			throws IOException
+	{
 		System.out.println("Standalone Ecommerce App\n");
 		System.out.println("1.REGISTER");
 		System.out.println("2.LOGIN");
@@ -20,12 +50,28 @@ public class CoreJavaStandAloneShoppingApp {
 				new InputStreamReader(System.in));
 		String input = reader.readLine();
 		
-		System.out.println(input);
-
+		if (user.getEmail() == null)
+		{
+			Register();
+		}
+		
+		else {
+			switch(input) {
+				case "1":
+					Register();
+					break;
+				default:
+					System.out.println("");
+			}
+		}
 	}
 	
-	public static void Register() {
+	public static class Users {
+		public static ArrayList<User> AllUsers = new ArrayList<User>();
 		
+		public static void addUser(User user) {
+			AllUsers.add(user);
+		}
 	}
-
+	
 }
---
+++ src/CoreJavaStandAloneShoppingApp/User.java
@@ -0,0 +1,24 @@
+package CoreJavaStandAloneShoppingApp;
+
+public class User {
+	private String email;
+	private String password;
+	
+	public User() {
+	
+	}
+	
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+
+}
